8d733ada380e6efa3311bbffbea0ccd8d1a45339
Kanchan Jaiswal <91010576+kanchanCode@users.noreply.github.com>
2021-11-11 16:14:00 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace regexPractice
+{
+    public class regexProblems
+    {
+
+        public static string firstName = "^[A-Z]{1}[a-zA-Z]{3,}$";
+        public static string lastName = "^[A-Z]{1}[a-zA-Z]{3,}$";
+        public static string email = "^[a-zA-Z]{3,}([.|#|+|_|-]?[a-zA-Z0-9]+)?[@]{1}[a-zA-Z0-9]+([.]{1}[a-zA-Z]{2,4})?[.]{1}[a-zA-Z]{2,4}$";
+        public static string mobileNo = "^[1-9]{2}[ ][1-9]{1}[0-9]{9}$";
+        public static string password = "^.*(?=.{8,})(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$";
+        //"^(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|])(?=.[^[*.!@$%^&(){}[]:;
+        //(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.[*.!@$%^&(){}[]:;<>,.?/~_+-=|]).{8,32}$";
+        // naresh shared[A-Z]{1}[0-9]{1} ([!|@|#|$|%|^|&|*|_|.|-]){1}[A-Za-z]{5,}$
+        // worked "^.*(?=.{8,})(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]){1}$"
+        // sir gave "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^$@!#%*?&]*[$#@!%*?&][^$@!#%*?&]*$).{8,}$
+
+
+        public bool Check(string input, int n)
+        {
+            if (n == 1)
+            {
+             
+                return Regex.IsMatch(input, firstName);
+            }
+            if (n == 2)
+            {
+                return Regex.IsMatch(input, lastName);
+            }
+            if (n == 5)
+            {
+                return Regex.IsMatch(input, password);
+            }
+            if (n == 4)
+            {
+                return Regex.IsMatch(input, mobileNo);
+            }
+            else
+            {
+                return Regex.IsMatch(input, email);
+            }
+
+        }
+    }
+    class Program 
+    { 
+        static void Main(string[] args)
+        {
+            regexProblems obj1 = new regexProblems();
+
+
+
+            Console.WriteLine(" 1. First name  \n 2. Last name  \n 3. email  \n 4. Mobile no with country code \n 5. password  \n");
+            Console.WriteLine("Enter your choice");
+            int n = Convert.ToInt32(Console.ReadLine());
+            switch (n)
+            {
+                case 1:
+                    Console.Write("Raghav ");
+                    Console.Write(obj1.Check("Raghav", 1));
+                    break;
+                case 2:
+                    Console.Write("Janki ");
+                    Console.Write(obj1.Check("Janki", 2));
+                    break;
+                case 3:
+                    Console.Write("abc.100@gmail.co.in ");
+                    Console.Write(obj1.Check("abc.100@gmail.co.in", 3));
+                    break;
+                case 4:
+                    Console.WriteLine("91 7654321890 ");
+                    Console.Write(obj1.Check("91 7654321890", 4));
+                    break;
+                case 5:
+                    Console.WriteLine("Passw@rd1 ");
+                    Console.WriteLine(obj1.Check("Passw@rd1", 5));
+                    break;
+                default:
+                    Console.WriteLine("invalid choice");
+                    break;
+
+            }
+        }
+    }
+
+
+}
---
+++ regexPattern.cs
@@ -0,0 +1,85 @@
+﻿/*using regexPractice;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace regexPractice
+{
+    public class regexPattern
+    {
+
+
+        public static string pincodeUC1 = "^[1-9][0-9]{5}$";
+        public static string pincodeUC2 = "^!([A-Z][.|!|@|#|$|%|^|&|*]?)[1-9][0-9]{5}$";
+        public static string pincodeUC3 = "^[1-9][0-9]{5}!([A-Z][.|!|@|#|$|%|^|&|*]?)$";
+        public static string pincodeUC4 = "^[1-9][0-9]{2}[  ][0-9]{3}$";
+        public static string emailValidation = "^[a-zA-Z]{3,}([.|#|+|_|-]?[a-zA-Z0-9]+)?[@]{1}[a-zA-Z0-9]+([.]{1}[a-zA-Z]{2,4})?[.]{1}[a-zA-Z]{2,4}$";
+
+
+        public bool InputCheck(string input, int n)
+        {
+            if (n == 1)
+            {
+                return Regex.IsMatch(input, pincodeUC1);
+            }
+            if (n == 2)
+            {
+                return Regex.IsMatch(input, pincodeUC2);
+            }
+            if (n == 5)
+            {
+                return Regex.IsMatch(input, emailValidation);
+            }
+            if (n == 4)
+            {
+                return Regex.IsMatch(input, pincodeUC4);
+            }
+            else
+            {
+                return Regex.IsMatch(input, pincodeUC3);
+            }
+
+        }
+
+
+
+    }
+
+
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            regexPattern obj1 = new regexPattern();
+            Console.WriteLine(" 1. Pincode UC1 \n 2. Pincode UC2 \n 3. Pincode UC3 \n 4. Pincode UC4 \n 5. email Validation \n");
+            Console.WriteLine("Enter your choice");
+            int n = Convert.ToInt32(Console.ReadLine());
+            switch (n)
+            {
+                case 1:
+                    Console.WriteLine(obj1.InputCheck("400088", 1));
+                    break;
+                case 2:
+                    Console.WriteLine(obj1.InputCheck("A400088", 2));
+                    break;
+                case 3:
+                    Console.WriteLine(obj1.InputCheck("400088B", 3));
+                    break;
+                case 4:
+                    Console.WriteLine(obj1.InputCheck("400 088", 4));
+                    break;
+                case 5:
+                    Console.WriteLine(obj1.InputCheck("abc.100@gmail.co.in", 5));
+                    break;
+                default:
+                    Console.WriteLine("invalid choice");
+                    break;
+
+            }
+        }
+    }
+
+}
+*/
