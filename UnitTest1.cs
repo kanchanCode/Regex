d1e5b3dc81225007b7d00e01b1e3de219d852d41
Kanchan Jaiswal <91010576+kanchanCode@users.noreply.github.com>
2021-11-11 17:13:03 +0530

    NUnit test_case

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,18 +1,27 @@
 using NUnit.Framework;
+using regexPractice;
 
 namespace TestProject
 {
     public class Tests
     {
+        public string patternforName="[A-Za-z]{3}";
+       
+        regexPattern obj;
+
         [SetUp]
         public void Setup()
         {
+            obj = new regexPattern();
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var result = obj.NameCheck("Kan", patternforName);
+            Assert.AreEqual(true,result.Success);
         }
+
+       
     }
 }
